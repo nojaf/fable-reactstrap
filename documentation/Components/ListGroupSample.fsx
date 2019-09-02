e5e2a4c209bccdf07211b3ae7e37ba9f65274eae
Derek Liang <derek.liang.ca@gmail.com>
2019-09-02 13:42:17 -0700

    add list group sample

--- documentation/Components/ListGroupSample.fsx
+++ documentation/Components/ListGroupSample.fsx
@@ -1,11 +1,28 @@
 #load "../../.paket/load/netstandard2.0/main.group.fsx"
 #load "../../src/Common.fs"
 #load "../../src/ListGroup.fs"
+#load "../../src/ListGroupItem.fs"
 
 open Fable.Core.JsInterop
 open Fable.React
 open Reactstrap
 
-let private listGroupSample = FunctionComponent.Of<obj>((fun _ -> fragment [] []), "ListGroupSample")
+let private listGroupSample =
+    FunctionComponent.Of<obj>((fun _ ->
+        fragment [] [
+
+            ListGroup.listGroup [ ]
+                [ ListGroupItem.listGroupItem [ ]
+                    [ str "Cras justo odio" ]
+                  ListGroupItem.listGroupItem [ ]
+                    [ str "Dapibus ac facilisis in" ]
+                  ListGroupItem.listGroupItem [ ]
+                    [ str "Morbi leo risus" ]
+                  ListGroupItem.listGroupItem [ ]
+                    [ str "Porta ac consectetur ac" ]
+                  ListGroupItem.listGroupItem [ ]
+                    [ str "Vestibulum at eros" ] ]
+        ])
+        , "ListGroupSample")
 
 exportDefault listGroupSample
