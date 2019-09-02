4a751e2f97c8b8cf43a1549ca60ad14c4b6ca5e7
Derek Liang <derek.liang.ca@gmail.com>
2019-09-02 15:30:20 -0700

    fix layout sample

--- documentation/Components/LayoutSample.fsx
+++ documentation/Components/LayoutSample.fsx
@@ -9,78 +9,68 @@ open Fable.React
 open Fable.React.Props
 open Reactstrap
 
-let container = Container.container
-let row = Row.row
-let col = Col.col
-let custom  attr =
-    attr
-    |> Col.Custom
-
 let private layoutSample =
     let customLayout =
-        {|
-            size = 6;
-            order = 2;
-            offset = 1;
-        |}
+        Col.mkCol !^ 6
+        |> Col.withOrder !^ 2
+        |> Col.withOffset !^ 1
 
     let customLayout1 =
-        {|
-            size = 6;
-            offset = 3;
-        |}
+        Col.mkCol !^ 6
+        |> Col.withOffset !^ 3
+
     let customLayout2 =
-        {|
-            size = "auto";
-            offset = 1;
-        |}
+        Col.mkCol !^ "auto"
+        |> Col.withOffset !^ 1
 
     FunctionComponent.Of<obj>
         ((fun _ -> fragment [] [
-            container [ ]
-                [ row [ ]
-                    [ col [ ] [str ".col" ]]
-                  row [ ]
-                    [ col [ ] [str ".col"]
-                      col [ ] [str ".col"]
-                      col [ ] [str ".col"]
-                      col [ ] [str ".col"] ]
-                  row [ ]
-                    [ col [ custom [ HTMLAttr.Custom("xs","3")] ]
+            Container.container [ ]
+                [ Row.row [ ]
+                    [ Col.col [ ] [str ".col" ]]
+                  Row.row [ ]
+                    [ Col.col [ ] [str ".col"]
+                      Col.col [ ] [str ".col"]
+                      Col.col [ ] [str ".col"]
+                      Col.col [ ] [str ".Col.col"] ]
+                  Row.row [ ]
+                    [ Col.col [ Col.Xs (Col.mkCol !^3) ]
                         [str ".col-3"]
-                      col [ custom [ HTMLAttr.Custom("xs", "auto") ] ]
+                      Col.col [ Col.Xs (Col.mkCol !^ "auto") ]
                         [str ".col-auto - variable width content"]
-                      col [ custom [ HTMLAttr.Custom("xs", "3") ] ]
+                      Col.col [ Col.Xs (Col.mkCol !^3) ]
                         [str ".col-3" ] ]
-                  row [ ]
-                    [ col [ custom [HTMLAttr.Custom ("xs", "6") ] ]
+                  Row.row [ ]
+                    [ Col.col [ Col.Xs (Col.mkCol !^6) ]
                         [str ".col-6"]
-                      col [ custom [HTMLAttr.Custom ("xs", "6") ] ]
+                      Col.col [ Col.Xs (Col.mkCol !^6) ]
                         [str ".col-6"] ]
-                  row [ ]
-                    [ col [ custom [HTMLAttr.Custom ("xs", "6")
-                                    HTMLAttr.Custom ("sm", "4")  ] ]
+                  Row.row [ ]
+                    [ Col.col [ Col.Xs (Col.mkCol !^6)
+                                Col.Sm (Col.mkCol !^4)
+                              ]
                         [ str ".col-6 .col-sm-4"]
-                      col [ custom [HTMLAttr.Custom ("xs", "6")
-                                    HTMLAttr.Custom ("sm", "4") ] ]
+                      Col.col [ Col.Xs (Col.mkCol !^6)
+                                Col.Sm (Col.mkCol !^4) ]
                         [str ".col-6 .col-sm-4"]
-                      col [custom  [HTMLAttr.Custom ("sm", "4") ] ]
+                      Col.col [ Col.Sm (Col.mkCol !^4) ]
                         [str ".col-sm-4" ] ]
-                  row [ ]
-                    [ col [ custom  [
-                            HTMLAttr.Custom ("sm", customLayout) ] ]
+                  Row.row [ ]
+                    [ Col.col [ Col.Custom  [ HTMLAttr.Custom ("sm", customLayout) ] ]
                         [ str ".col-sm-6 .order-sm-2 .offset-sm-1"]
                      ]
-                  row [ ]
-                    [ col [ custom [
-                            HTMLAttr.Custom ("md", customLayout1)
-                            HTMLAttr.Custom ("sm", "12") ] ]
+                  Row.row [ ]
+                    [ Col.col [ Col.Custom [
+                                    HTMLAttr.Custom ("md", customLayout1)
+                                ]
+                                Col.Sm (Col.mkCol !^12)
+                              ]
                         [ str ".col-sm-12 .col-md-6 .offset-md-3" ]
                     ]
-                  row [ ]
-                    [ col [ custom [ HTMLAttr.Custom ("sm", customLayout2) ] ]
+                  Row.row [ ]
+                    [ Col.col [ Col.Custom [ HTMLAttr.Custom ("sm", customLayout2) ] ]
                         [str ".col-sm-auto .offset-sm-1"]
-                      col [ custom [ HTMLAttr.Custom ("sm", customLayout2) ] ]
+                      Col.col [ Col.Custom [ HTMLAttr.Custom ("sm", customLayout2) ] ]
                         [str ".col-sm-auto .offset-sm-1"]
                     ]
                 ]
