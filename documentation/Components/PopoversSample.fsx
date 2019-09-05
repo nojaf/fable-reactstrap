7a0556e799415ccf6d5df15732b3eeb4d22d9dcd
Derek Liang <derek.liang.ca@gmail.com>
2019-09-05 08:36:46 -0700

    add popover sample

--- documentation/Components/PopoversSample.fsx
+++ documentation/Components/PopoversSample.fsx
@@ -1,11 +1,38 @@
 #load "../../.paket/load/netstandard2.0/main.group.fsx"
 #load "../../src/Common.fs"
-#load "../../src/Popovers.fs"
+#load "../../src/Popover.fs"
+#load "../../src/PopoverHeader.fs"
+#load "../../src/PopoverBody.fs"
 
 open Fable.Core.JsInterop
 open Fable.React
+open Fable.React.Props
 open Reactstrap
 
-let private popoversSample = FunctionComponent.Of<obj>((fun _ -> fragment [] []), "PopoversSample")
+let private popoversSample =
+    FunctionComponent.Of<obj>
+        ((fun _ ->
+        let showPopover = Hooks.useState(false)
+        let toggleShowPopover = fun _ -> showPopover.update(not showPopover.current)
+        fragment []
+            [
+            button [ Id "Popover1"
+                     Type "button" ]
+                [ str "Launch Popover" ]
+            Popover.popover
+                [
+                    Popover.Placement Bottom
+                    // HTMLAttr.Custom ("placement", "bottom")
+                    // HTMLAttr.Custom ("isopen", "{this.state.popoverOpen}")
+                    Popover.IsOpen showPopover.current
+                    Popover.Target !^ "Popover1"
+                    Popover.Toggle toggleShowPopover
+                        // HTMLAttr.Custom ("toggle", "{this.toggle}")
+                        ]
+                [ PopoverHeader.popoverHeader [ ]
+                    [ str "Popover Title" ]
+                  PopoverBody.popoverBody [ ]
+                    [ str "Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum." ] ]
+            ]), "PopoversSample")
 
 exportDefault popoversSample
