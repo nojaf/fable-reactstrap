acfdfd80ea37cb61fb588b30c261c522905a7b36
Derek Liang <derek.liang.ca@gmail.com>
2019-09-04 20:51:23 -0700

    add spinner sample

--- documentation/Components/SpinnersSample.fsx
+++ documentation/Components/SpinnersSample.fsx
@@ -1,11 +1,50 @@
 #load "../../.paket/load/netstandard2.0/main.group.fsx"
 #load "../../src/Common.fs"
-#load "../../src/Spinners.fs"
+#load "../../src/Spinner.fs"
 
 open Fable.Core.JsInterop
 open Fable.React
 open Reactstrap
 
-let private spinnersSample = FunctionComponent.Of<obj>((fun _ -> fragment [] []), "SpinnersSample")
+let private spinnersSample =
+    FunctionComponent.Of<obj>
+        ((fun _ ->
+            fragment []
+                [
+                  Spinner.spinner [ Spinner.Color Primary ]
+                    [ ]
+                  Spinner.spinner [ Spinner.Color Secondary]
+                    [ ]
+                  Spinner.spinner [ Spinner.Color Success]
+                    [ ]
+                  Spinner.spinner [ Spinner.Color Danger]
+                    [ ]
+                  Spinner.spinner [ Spinner.Color Warning]
+                    [ ]
+                  Spinner.spinner [ Spinner.Color Info]
+                    [ ]
+                  Spinner.spinner [ Spinner.Color Light]
+                    [ ]
+                  Spinner.spinner [ Spinner.Color Dark]
+                    [ ]
+                  br []
+                  Spinner.spinner [ Spinner.Type "grow"; Spinner.Color Primary ]
+                    [ ]
+                  Spinner.spinner [ Spinner.Type "grow"; Spinner.Color Secondary]
+                    [ ]
+                  Spinner.spinner [ Spinner.Type "grow"; Spinner.Color Success]
+                    [ ]
+                  Spinner.spinner [ Spinner.Type "grow"; Spinner.Color Danger]
+                    [ ]
+                  Spinner.spinner [ Spinner.Type "grow"; Spinner.Color Warning]
+                    [ ]
+                  Spinner.spinner [ Spinner.Type "grow"; Spinner.Color Info]
+                    [ ]
+                  Spinner.spinner [ Spinner.Type "grow"; Spinner.Color Light]
+                    [ ]
+                  Spinner.spinner [ Spinner.Type "grow"; Spinner.Color Dark]
+                    [ ]
+                ]
+        ), "SpinnersSample")
 
 exportDefault spinnersSample
